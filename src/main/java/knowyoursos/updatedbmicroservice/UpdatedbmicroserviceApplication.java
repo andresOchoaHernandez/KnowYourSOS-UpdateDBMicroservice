731221cdffc11c8820a7695169e70efb70962609
andresOchoaHernandez <ada.wong.gv@gmail.com>
2025-05-27 23:17:53 +0200

    Initial commit

---
+++ src/main/java/knowyoursos/updatedbmicroservice/UpdatedbmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package knowyoursos.updatedbmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdatedbmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdatedbmicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/knowyoursos/updatedbmicroservice/UpdatedbmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package knowyoursos.updatedbmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdatedbmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
